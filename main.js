c8b60e23d1a22bdd40d6ca31c9a04849dcd6628d
J-unStiN <junstin119@gmail.com>
2020-01-03 10:19:04 +0900

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function hello(params) {
-    console.log("Hello node");
+function hello(aaa) {
+    aaa = "121212"
+    console.log("Hello node"+aaa);
 }
 
-hello();
+hello(33);
+
+// 하루 1커밋이라도 하는게 정신이다.
