3cd0e8f3313dbfe5ad37b4e92669820f5917e31e
J-unStiN <junstin119@gmail.com>
2020-01-04 23:58:08 +0900

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-function hello(aaa) {
-    aaa = "121212"
-    console.log("Hello node"+aaa);
+function hello() {
+    str1 = "121212"
+    str2 = "asjdkasjdk"
+    console.log("Hello node"+str1+str2);
+
+    var a = 1, b = 2, c = 3, d = 4;
+
+    // 비교연산자
+    a += 1;  //  a = a + 1; 과 같다 
+    b -= 1;  //  b = b - 1; 과 같다 
+    c *= 2;  //  c = c * 2; 과 같다 
+    d /= 2;  //  d = d / 2; 과 같다 
+
+    console.log(a,b,c,d)
 }
 
-hello(33);
+hello();
 
 // 하루 1커밋이라도 하는게 정신이다.
