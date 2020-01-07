049ec8686604d2135a59255847e418941c3750bf
J-unStiN <56985486+J-unStiN@users.noreply.github.com>
2020-01-07 17:47:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,50 @@ function hello() {
     console.log(result);
     // 조건문
 
+    //반복문 for문
+    var array = new Array();
 
+    for(var i=0; i<10; i++){
+        array.push(i);
+    }
+    console.log(array.toString());
+
+
+    // for in 문
+    var a = ['a', 'b', 'c', 'x', 'y', 'z'];
+    var result = '';
+
+    for (var i in a) {   
+        result += 'index:'+ i + ', value:'+ a[i] + '\n'; 
+    }
+
+    console.log(result);
+    //반복문 for문
+
+    //반복문 while
+    var i = 0 ;
+    do {
+        i++;
+    }while(i<10)
+    console.log(i)
+    //반복문 while
+    
+    
+    
+/*
+함수
+함수를 선언할 때는 function 이라는 키워드를 사용하며 
+별도로 함수의 반환값에 대한 자료형은 명시하지 않아도 됩니다.
+*/
+
+    function sum(a,b) {
+        var c = a + b;
+        return c;
+    }
+    
+    var add = sum;
+    add(3,3);
+    
 }
 
 hello();
