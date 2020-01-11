e2557f85a0ae017edf7b9b8614c7a0ee9771c0e0
J-unStiN <56985486+J-unStiN@users.noreply.github.com>
2020-01-11 23:56:32 +0900

    Update nodenode.js

--- nodenode.js
+++ nodenode.js
@@ -1,3 +1,4 @@
+/*
 var server = require('http');
 
 server.createServer(function(req, res){
@@ -6,3 +7,14 @@ server.createServer(function(req, res){
 }).listen(3000, 'localhost');
 
 console.log('Server is running at http://localhost:3000/');
+*/
+
+// 파일 읽는법!!
+var fs = require("fs"); 
+
+fs.readFile('./test_txt.txt', encoding = 'utf-8', function(err, data) {
+	if (err) {
+		throw err;
+	}
+	console.log(data);
+});
